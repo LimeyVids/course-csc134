14dfc5acd913101431745608d7e5ba0f38f3b284
Liam <155418855+LimeyVids@users.noreply.github.com>
2026-08-19 11:09:34 -0400

    Uploaded M1T1

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// One line comment
+/*
+Block comment
+M1T1 - Hello World
+Sanderson
+8/19/26
+First program - just a smoke test.
+*/
+
+// magic words to start up
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello CSC 134!" << endl;
+    // name is a VERIABLE - a noun
+    string name;
+    cout << "What is your name? ";
+    cin  >> name; // input - reads the name into this variable
+
+    cout << "This program is for: " << name << endl;
+    return 0; // no errors
+}
